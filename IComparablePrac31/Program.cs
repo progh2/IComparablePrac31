ee9764375d53ab96a272a499c094a4abbae1c76a
Gihun Ham <ham@e-mirim.hs.kr>
2023-09-07 14:03:14 +0900

    Product 객체를 정렬해서 보여주는 코드

--- IComparablePrac31/Program.cs
+++ IComparablePrac31/Program.cs
@@ -6,10 +6,30 @@ using System.Threading.Tasks;
 
 namespace IComparablePrac31
 {
-    internal class Program
+    class Product
+    {
+        public string Name { get; set; }
+        public int Price{ get; set; }
+        public override string ToString()
+        {
+            return Name + " : " + Price + "원";
+        }
+        internal class Program
     {
         static void Main(string[] args)
         {
+                List<Product> products = new List<Product>()
+                {
+                    new Product(){Name="고구마", Price=1500 },
+                    new Product(){Name="사과", Price=2400 },
+                    new Product(){Name="바나나", Price=1000 },
+                    new Product(){Name="배", Price=3000 },
+                };
+                products.Sort();
+                foreach(Product p in products)
+                {
+                    Console.WriteLine(p);
+                }
         }
     }
 }
